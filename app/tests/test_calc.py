9cf2ff995306837a1107982dd8a9cf74f8381376
kenfelix <Kmrapper.kf@gmail.com>
2023-03-27 01:32:17 +0100

    front end

--- app/tests/test_calc.py
+++ app/tests/test_calc.py
@@ -1,11 +1,11 @@
+from app.src import calc
 import unittest
-from ..calc import is_normal
 
 class TestCalc(unittest.TestCase):
     
     def test_solve_linear_equation(self):
-        result1 = is_normal("7x-2=21")
-        result2 = is_normal("2(4x + 3) + 6 =24 - 4x")
+        result1 = calc.is_normal("7x-2=21")
+        result2 = calc.is_normal("2(4x + 3) + 6 =24 - 4x")
         
         self.assertEqual(result1, True)
         self.assertEqual(result2, False)
---
+++ front-end/app/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+import foondamatelogo from '../public/foondamatelogo.webp'
+import { useEffect, useState } from 'react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const register = () => {
+    fetch('/user')
+  }
+
+  const login = () => {
+
+    const opts = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        "username": email,
+        "password": password
+      })
+    }
+
+    fetch('/login', opts)
+      .then(resp => {
+        if (resp.status === 200) return resp.json();
+        else alert("Invalid credentials")
+      })
+      .then()
+      .catch(error => {
+        console.error("Isomething went wrong", error)
+      })
+  }
+  // useEffect(() => {
+  //   fetch()
+  // })
+  return (
+    <div className='p-[20px] flex flex-col items-center space-y-24'>
+      <div className='flex flex-row justify-between items-center space-x-[7rem]'>
+        <Image src={foondamatelogo} alt={'logo'} className='h-[50px] w-[120px]' />
+        <form className='flex flex-row justify-between items-center space-x-3'>
+          <input type="email" placeholder='test@email.com' value={email} onChange={(e) => setEmail(e.target.value)} className='bg-slate-300 border-1 border-solid border-purple-600 rounded-[16px] py-2 px-4' />
+          <input type="password" placeholder='********' value={password} onChange={(e) => setPassword(e.target.value)} className='bg-slate-300 border-1 border-solid border-purple-600 rounded-[16px] py-2 px-4' />
+          <input type="submit" value="Register" onClick={register} className='bg-purple-600 rounded-[16px] py-2 px-4 text-white cursor-pointer' />
+          <input type="submit" value="Login" onClick={login} className='bg-purple-600 rounded-[16px] py-2 px-4 text-white cursor-pointer' />
+        </form>
+      </div>
+      <div>
+        <form className='flex flex-row justify-between items-center space-x-3'>
+          <input type="text" placeholder='input equation (e.g 7x-2=21 or 2(4x + 3)+3=24-4x)' className='bg-slate-300 border-1 border-solid border-purple-600 rounded-[16px] py-2 px-4' />
+          <input type="submit" value="Solve" className='bg-purple-600 rounded-[16px] py-2 px-4 text-white cursor-pointer' />
+        </form>
+      </div>
+    </div>
+  )
+}
